3ac78ebb638509878d199b21642ee93960be2b0b
sagar <anandguptasir@gmail.com>
2022-12-12 13:31:31 +0530

    initial commit

--- src/component/newpage.jsx
+++ src/component/newpage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const newpage = () => {
+   // const makepayment = (token) => {
+    // const history =useNavigate();
+    // const [product, setproduct] = useState({
+    //   name: "YOGA SUBSCRIPTION",
+    //   price: 500,
+    //   prodcutby: "YOGA",
+    //   currency:"inr"
+    // });
   return (
-    <div>newpage</div>
+    <div>  {/* <StripeCheckout className="bg-amber-500"
+    stripeKey="pk_test_51LQDmPSJatPmI7e7Xr3j0tHYPmhZqi7Uy24T5Op28XUx1tpvjzdC5mKWy4eRDksHFkDoi97XoJOjZWaQ6CrQwjw300pCvIecD0"
+    token={makepayment}
+    name={product.name}
+    amount={product.price * 100}
+    currency={product.currency}
+    ><button type="submit" className="mt-[2rem] p-1 tracking-[0.2rem] ml-[4rem] rounded-[1rem] bg-black text-white w-[12rem]  font-semibold">Pay Now</button></StripeCheckout> */}</div>
   )
 }
 
